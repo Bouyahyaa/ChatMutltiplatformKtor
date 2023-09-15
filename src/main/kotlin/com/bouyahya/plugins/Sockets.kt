7fb09ac21e8edf58407d3dcf605c99eae4fb46b7
Bilel_Bouyahyaa <bilel.bouyahya@outlook.com>
2023-09-15 21:15:04 +0100

    connected socket

--- src/main/kotlin/com/bouyahya/plugins/Sockets.kt
+++ src/main/kotlin/com/bouyahya/plugins/Sockets.kt
@@ -3,6 +3,7 @@ package com.bouyahya.plugins
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
+import io.ktor.websocket.*
 import java.time.Duration
 
 fun Application.configureSockets() {
@@ -16,6 +17,18 @@ fun Application.configureSockets() {
     routing {
         webSocket("/chat") {
             println("Connected")
+            try {
+                send("You are connected!")
+                for (frame in incoming) {
+                    frame as? Frame.Text ?: continue
+                    val receivedText = frame.readText()
+                    println(receivedText)
+                }
+            } catch (e: Exception) {
+                println(e.localizedMessage)
+            } finally {
+                println("Removing!")
+            }
         }
     }
 }
